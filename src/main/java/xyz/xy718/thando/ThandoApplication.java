34b53830ca55f66a97ec33dfee19d58d36081803
Xy718 <jin@xy718.xyz>
2022-06-23 23:46:28 +0800

    init

---
+++ src/main/java/xyz/xy718/thando/ThandoApplication.java
@@ -0,0 +1,13 @@
+package xyz.xy718.thando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThandoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThandoApplication.class, args);
+    }
+
+}
